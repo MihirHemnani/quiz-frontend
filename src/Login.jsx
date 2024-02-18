d2cf6d1e1cb5826352a3a63c1a711c603104491a
Hemnani Mihir Jitumal <mihir.hemnani2336.gmail.com>
2024-02-18 16:50:27 +0530

    timer

--- src/Login.jsx
+++ src/Login.jsx
@@ -8,6 +8,7 @@ const Login = () => {
   // State to manage form input values
   const [leaderboard, setLeaderBoard] = useState(false);
   const [errmsg, setErr] = useState("")
+  const [pop, setPop] = useState(false)
   
 
   const [formData, setFormData] = useState({
@@ -26,6 +27,92 @@ const Login = () => {
       [name]: value,
     }));
   };
+
+  const Popup = () => {
+    return (
+    <div className="quiz-container" style={{maxWidth:"50vh",maxHeight: "50vh", marginTop: "20vh", overflow: "auto", scrollbarWidth: "none"}}>
+
+      <h2 style={{textAlign: "center"}}>Terms & Conditions</h2>
+
+      <p class=" fs-3">1.	The Unmaad Virtual Expedition is open to participants of all ages.
+      </p>
+
+      <p class=" fs-3">
+          2.	Participants must register on unmaad.com to be eligible for the event.
+      </p>
+
+      <p class=" fs-3">
+          3.	Participants must provide accurate and complete information during registration.
+      </p>
+
+      <p class=" fs-3">
+          4.	Each participant can register only once.
+      </p>
+
+      <p class=" fs-3">
+          5.	The event will be hosted exclusively on unmaad.com
+      </p>
+
+      <p class=" fs-3">
+          6.	Questions will be presented in image format, and participants are required to enter their answers in the provided text field.
+      </p>
+
+      <p class=" fs-3">
+          7.	Participants must adhere to fair play and ethical conduct throughout the event.
+      </p>
+
+      <p class=" fs-3">
+          8.	Any form of cheating, plagiarism, or use of external assistance is strictly prohibited and may lead to disqualification.
+      </p>
+
+      <p class=" fs-3">
+          9.	Participants are responsible for submitting their answers within the specified time frame.
+      </p>
+
+      <p class=" fs-3">
+          10.	Correct answers will be awarded points based on the scoring system outlined on unmaad.com.
+      </p>
+
+      <p class=" fs-3">
+          11.	The organizer's decision on scoring and winners is final and binding.
+      </p>
+
+      <p class=" fs-3">
+          12.	Prizes will be awarded to the participants with the highest scores.
+      </p>
+
+      <p class=" fs-3">
+          13.	Prize distribution details will be communicated on the event website.
+      </p>
+
+      <p class=" fs-3">
+          14.	Unmaad is not responsible for any technical issues, including but not limited to internet connectivity problems, server downtimes, or platform malfunctions.
+      </p>
+
+      <p class=" fs-3">
+          15.	Unmaad reserves the right to modify event rules, format, or any other aspect of the Virtual Expedition at its discretion.
+      </p>
+
+      <p class=" fs-3">
+          16.	Unmaad reserves the right to disqualify any participant who violates the terms and conditions or engages in any form of misconduct.
+      </p>
+
+      <p class=" fs-3">
+          17.	Unmaad is not liable for any loss, injury, or damages incurred by participants during or as a result of the Virtual Expedition.
+      </p>
+
+      <p class=" fs-3">
+          18.	By participating in the Unmaad Virtual Expedition, participants acknowledge and agree to abide by these terms and conditions.
+      </p>
+      
+      <div style={{textAlign: "center"}}><button onClick={() => handlePopup()}>Go Back</button></div>
+    </div>
+  )
+  }
+
+  const handlePopup = () => {
+    setPop(!pop)
+  }
 
   // Handle form submission
   const handleSubmit = (e) => {
@@ -61,6 +148,10 @@ const Login = () => {
   }
 
   return (
+    pop 
+    ?
+    <Popup/> 
+    :
     <div className="quiz-container" style={{width: "max-content", marginTop: "20vh"}}>
       {
       leaderboard ? 
@@ -68,7 +159,9 @@ const Login = () => {
         <LeaderBoard />
       </>  
       :
-      (<>
+      (
+        <>
+        
         <span className="active-question-no" style={{margin: "auto"}}>Set Your Username</span>
         <p style={{color: "red"}}>{errmsg}</p>
 
@@ -103,6 +196,23 @@ const Login = () => {
               onChange={handleInputChange}
               required
             />
+            <div style={{display: "flex"}}>
+              <div style={{marginRight: "1vh"}}>
+                <input
+                  style={{margin: "2vh 0"}}
+                  type="checkbox"
+                  name="collegeName"
+                  value="Terms and Conditions"
+                  required
+                />
+              </div>
+              <div style={{margin: "2vh 0"}}>
+                <a href='#' onClick={handlePopup}>
+                  Terms and Conditions
+                </a>
+              </div>
+            </div>
+            
           </div>
 
           <div className="footer">
